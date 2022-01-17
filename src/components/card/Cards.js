b652a230bbe2b5c27bad9a72d1480485195fafdc
이종민 <jongminlee0218@feelway.com>
2022-01-17 16:21:25 +0900

    feat : Add Swipe Card

--- src/components/card/Cards.js
+++ src/components/card/Cards.js
@@ -1,6 +1,14 @@
 import '../../css/Cards.css';
 import { Link } from 'react-router-dom';
-import defaultImage from '../../default-image.jpeg'
+import defaultImage from '../../default-image.jpeg';
+import {
+    SwipeableList,
+    SwipeableListItem,
+    SwipeAction,
+    TrailingActions,
+    Type
+} from 'react-swipeable-list';
+import 'react-swipeable-list/dist/styles.css';
 
 const Cards = ({data, onClickCard, disableDelete=false}) => {
 
@@ -12,22 +20,35 @@ const Cards = ({data, onClickCard, disableDelete=false}) => {
         e.target.src = defaultImage;
     }
 
+    const trailingActions = () => (
+        <TrailingActions>
+            <SwipeAction onClick={()=>{}}>
+                {disableDelete ? "" : <button className="btn" onClick={clickBtn} id={data.id}>DELETE</button>}
+            </SwipeAction>
+        </TrailingActions>
+    );
+
 
     return (    
-        <div className="card">
-            <a href={data.download_url} target="_blank">
-                <img src={data.download_url} alt={data.author} onError={handleImgError} />
-            </a>
-            <div className="container">
-                <Link to={"/detail/" + data.author + "/" + data.id} >
-                    <p className="author">{data.author}</p> 
-                </Link>
-                <a href={data.download_url} target="_blank">
-                    <p className="id">#{data.id}</p> 
-                </a>
-            </div>
-            {disableDelete ? "" : <button className="btn" onClick={clickBtn} id={data.id}>DELETE</button>}
-        </div>
+        <SwipeableList type={Type.IOS} fullSwipe={false}>
+            <SwipeableListItem
+                trailingActions={trailingActions()}
+            >
+                <div className="card">
+                    <a href={data.download_url} target="_blank">
+                        <img src={data.download_url} alt={data.author} onError={handleImgError} />
+                    </a>
+                    <div className="container">
+                        <Link to={"/detail/" + data.author + "/" + data.id} >
+                            <p className="author">{data.author}</p> 
+                        </Link>
+                        <a href={data.download_url} target="_blank">
+                            <p className="id">#{data.id}</p> 
+                        </a>
+                    </div>
+                </div>
+            </SwipeableListItem>
+        </SwipeableList>
     )
 }
 
